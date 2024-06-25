a82c2bb2e14a0d7d6d991c3e8bc54dd53fd395fd
Fadel Bidiga <bidigafadel@gmail.com>
2024-06-25 21:49:43 +0000

    first commit

---
+++ vars/slack_notification.groovy
@@ -0,0 +1,17 @@
+def call(String build_status = 'STARTED') {
+  build_status = build_status ?: 'SUCCESS'
+
+  def color
+
+  if (build_status == 'SUCCESS') {
+    color = '#47ec05'
+  } else if (build_status == 'UNSTABLE') {
+    color = '#d5ee0d'
+  } else {
+    color = '#ec2805'
+  }
+
+  def msg = "${build_status}: `${env.JOB_NAME}` #${env.BUILD_NUMBER}:\n${env.BUILD_URL}"
+
+  slackSend(color: color, message: message)
+}
